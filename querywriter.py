7e9cf9f47db1eb00616044038e8581432b3b3a5b
fhb1990 <freekboersma@gmail.com>
2020-01-23 14:54:10 +0100

    Removed unused py files

--- querywriter.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def writer(name, zoom, query, path=os.path.join(os.environ["HOMEPATH"], "Desktop")):
-    with open("{}/query_{}_zoomlevel{}.sql".format(path, name, zoom), "w") as fh:
-        fh.write(query)
