381b793d1223072d56dcd5c42e83fcf31cfe2c42
fhb1990 <freekboersma@gmail.com>
2020-01-27 11:25:17 +0100

    Commented sldextract

--- sldextract.py
+++ sldextract.py
@@ -35,11 +35,12 @@ def sld_to_rules(path):
             else:
                 max_scale = int(float(max_scale_el.text))
 
-            # Assume there is one Filter per Rule
+            # Assume there is max one Filter per Rule
             filt = rule.find('{*}Filter')
             filters = []
             logical = None
 
+            # If there is a filter
             if filt is not None:
 
                 # Check for And/Or, otherwise None
@@ -66,21 +67,34 @@ def get_filters(logic_string, sld_element):
     """
     Returns list of Filter objects per rule.
     """
+    # Initialize list of Filter objects
     filts = []
+
+    # For each triplet (logical, field, value)
     for sub_element in sld_element:
+
+        # Initialize triplet with logical
         args = [logic_string]
+
+        # Search for field/value combinations and add to triplet
         for binary in sub_element:
             if "PropertyName" in binary.tag:
                 args.append(binary.text)
             elif "Literal" in binary.tag:
                 args.append(binary.text)
+
+            # Only 'area' function supported for now
             elif "Function" in binary.tag:
                 if binary.attrib["name"] == "area":
                     args.append("ST_AREA({})".format(binary[0].text))
                 else:
                     raise Exception("Unknown function type found in Filter.")
+
+            # For arithmetic operators
             elif has_arithmetic(sub_element):
                 args.append(get_arithmetic(binary))
+
+            # Raise exception when SLD file is structured otherwise
             else:
                 raise Exception("SLD file not structured as expected.\n"
                                 "Functionality found not yet supported by this program.")
@@ -94,16 +108,23 @@ def get_arithmetic(sld_element):
     Returns arithmetic operator string for WHERE clause in query.
     """
     operators = {"Add": "+", "Sub": "-", "Mul": "*", "Div": "/"}
+
+    # Check what arithmetic operator is used
     for op in operators:
         if op in sld_element.tag:
             arith_op = operators[op]
+
     op_vals = []
     for element in sld_element:
         nested_arith = False
+
+        # If field/value again contains arithmetic operators, call recursion
         for op in operators:
             if op in element.tag:
                 op_vals.append(get_arithmetic(element))
                 nested_arith = True
+
+        # If the arithmetic is not nested, find the two values for the binary operator
         if not nested_arith:
             prop = element.find("{*}PropertyName")
             if "Literal" in element.tag:
@@ -118,8 +139,8 @@ def get_arithmetic(sld_element):
                     raise Exception("Unknown element structure found in arithmetic operator.")
             else:
                 op_vals.append(prop.text)
-                print(prop.text)
 
+    # Return string with arithmetic
     arith_str = "({} {} {})".format(op_vals[0], arith_op, op_vals[1])
     return arith_str
 
@@ -178,13 +199,20 @@ class Rule:
                  - "",      when the Rule contains no Filters
                  - string,  otherwise
         """
+        # Initialize WHERE clause
         clause = ""
+
+        # If the denominator is between min and max scale, give input for WHERE clause
         if self.min_scale <= input_denom <= self.max_scale:
             if len(self.filters) > 0:
+
+                # Without AND / OR
                 if self.logical is None:
                     clause += "({} {} {})".format(self.filters[0].field,
                                                   self.filters[0].logical_string,
                                                   self.filters[0].value)
+
+                # With AND / OR
                 else:
                     clause += "("
                     for fil in self.filters:
@@ -209,6 +237,7 @@ class Layer:
         """
         Returns WHERE clause for query, based on scale input.
         """
+        # Initialize booleans, query for layer
         query = "SELECT * FROM {} WHERE (".format(mapping[self.name])
         is_where_str = False
         is_where_pop = False
@@ -219,19 +248,28 @@ class Layer:
             if type(where_sub) is str:
                 is_where_str = True
 
+                # scale_select() did not return an empty string
                 if len(where_sub) > 0:
                     is_where_pop = True
 
                     # Prevent repetition in query (e.g. because of lines and fills)
                     if where_sub not in query:
+
+                        # Add to WHERE clause
                         query += rul.scale_select(input_denom)
                         query += " OR "
 
+        # If there is a populated WHERE clause, finalize query
         if is_where_pop:
             query = query[:-4]
             query += ")"
+
+        # If scale_select() returned "", query everything
         elif is_where_str:
             query = query[:-8]
+
+        # If scale_select() returned None, query nothing
         else:
             query += "False)"
+
         return query
