7d47caf6be31e4c6a1efe67fb6ef4d8a4fb70970
fhb1990 <freekboersma@gmail.com>
2020-01-29 10:51:35 +0100

    Added CLI in main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sldextract as se
-import sys
 import ast
+import argparse
 
 zoom_to_scale = {
     0: 500000000,
@@ -26,25 +26,67 @@ zoom_to_scale = {
     20: 500
 }
 
+
+def is_zoom_int(variable, string):
+    try:
+        int(variable)
+    except ValueError:
+        parser.error("--zoom{} should be an integer.".format(string))
+    else:
+        if 0 <= int(variable) <= 20:
+            return int(variable)
+        else:
+            parser.error("--zoom{} should be between 0 and 20.".format(string))
+
+
 if __name__ == "__main__":
+    # Initialize argparser
+    parser = argparse.ArgumentParser(description="Get queries for MVTs based on SLDs.")
+    parser.add_argument("-f", "--function", required=True,
+                        help="Can be 'getlayernames' or 'getqueries'. "
+                             "Function 'getlayernames' only requires --sld to be specified.")
+    parser.add_argument("-s", "--sld", required=True,
+                        help="Path or URL to SLD file.")
+    parser.add_argument("-l", "--layername",
+                        help="Specifies what layer to query. "
+                             "When not given, all NamedLayers are queried.")
+    parser.add_argument("-m", "--mapping",
+                        help="Dictionary that maps NamedLayers to database tables. "
+                             "Should be in the form \"{'key': 'value'}\".")
+    parser.add_argument("-z", "--zoommin",
+                        help="Optional argument, default value is 0.")
+    parser.add_argument("-x", "--zoommax",
+                        help="Optional argument, default value is 20.")
+    args = parser.parse_args()
 
     # Set default zoom values
     min_zoom, max_zoom = 0, 20
 
     # Print the names of the NamedLayers
-    if sys.argv[1] == "getlayernames":
-        sld = sys.argv[2]
-        for layer in se.sld_to_rules(sld):
+    if args.function == "getlayernames":
+        for layer in se.sld_to_rules(args.sld):
             print(layer.name)
 
     # Print the requested queries
-    elif sys.argv[1] == "getqueries":
-        sld = sys.argv[2]
-        mapping = ast.literal_eval(sys.argv[3])
-        if len(sys.argv[2:]) == 4:
-            min_zoom = int(sys.argv[4])
-            max_zoom = int(sys.argv[5])
-        for layer in se.sld_to_rules(sld):
-            for zl in range(min_zoom, max_zoom + 1):
-                query = layer.make_query(zoom_to_scale[zl], mapping)
-                print(query)
+    elif args.function == "getqueries":
+        if args.mapping is None:
+            parser.error("'getqueries' requires a Mapping attribute. Add a mapping with the '-m' or '--mapping' flag.")
+        else:
+            mapping = ast.literal_eval(args.mapping)
+
+            if args.zoommin is not None:
+                min_zoom = is_zoom_int(args.zoommin, "min")
+            if args.zoommax is not None:
+                max_zoom = is_zoom_int(args.zoommax, "max")
+            if min_zoom > max_zoom:
+                parser.error("--zoommax has to be greater than or equal to --zoommin")
+
+            layernames = [layer.name for layer in se.sld_to_rules(args.sld)]
+            if (args.layername is not None) and (args.layername not in layernames):
+                parser.error("Given --layername not present in SLD.")
+            else:
+                for layer in se.sld_to_rules(args.sld):
+                    if (args.layername is None) or (layer.name == args.layername):
+                        for zl in range(min_zoom, max_zoom + 1):
+                            query = layer.make_query(zoom_to_scale[zl], mapping)
+                            print(query)
